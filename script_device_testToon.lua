88763db1ecc701aaaef4f7eaf24d9eb96713796d
mphagenaars <mphagenaars@gmail.com>
2016-12-26 14:49:19 +0100

    implemented uuid4

--- script_device_testToon.lua
+++ script_device_testToon.lua
@@ -7,13 +7,14 @@ local pass = 'xxxxxxxxxx'
 local debug = 1
 
 -- UUID
-random = math.random
 function uuid()
-   local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-   return string.gsub(template, '[xy]', function (c)
-      local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
-      return string.format('%x', v)
-   end)
+   tmp = assert(io.popen('curl GET https://www.uuidgenerator.net/api/version4'))
+   uuid4 = tostring(tmp:read('*all'))
+   tmp:close()
+   if debug == 1 then 
+      print('uuid: '..uuid4)
+   end
+   return uuid4
 end
 
 -- LOGIN
@@ -35,8 +36,9 @@ function login()
    agreementIdChecksum = tostring(jsonLogin.agreements[1].agreementIdChecksum)
    if debug == 1 and jsonLogin.success == true then print('Eneco Toon - sleutel opgehaald') end
    -- step 2: log on to Toon api
-   local uuId = uuid()
-   local d2 = '\"clientId='..clientId..'&clientIdChecksum='..clientIdChecksum..'&agreementId='..agreementId..'&agreementIdChecksum='..agreementIdChecksum..'&random='..uuId..'\"'
+   local tmp1 = uuid()
+   if debug == 1 then print('login - uuid: '..tostring(uuid4)) end
+   local d2 = '\"clientId='..clientId..'&clientIdChecksum='..clientIdChecksum..'&agreementId='..agreementId..'&agreementIdChecksum='..agreementIdChecksum..'&random='..uuid4..'\"'
    if debug == 1 then print('body login: '..d2) end
    auth = assert(io.popen('curl -X POST -d ' .. d2 .. ' https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/start'))
       local check = auth:read('*all')
@@ -55,8 +57,9 @@ end
 
 -- LOGOUT
 function logout()
-   local uuId = uuid()
-   local d3 = '\"random='..uuId..'&clientId='..clientId..'&clientIdChecksum='..clientIdChecksum..'\"' 
+   local tmp2 = uuid()
+   if debug == 1 then print('logout - uuid: '..uuid4) end
+   local d3 = '\"random='..uuid4..'&clientId='..clientId..'&clientIdChecksum='..clientIdChecksum..'\"' 
    if debug == 1 then print('body logout: '..d3) end
    logout = assert(io.popen('curl -X POST -d ' .. d3 .. ' https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/logout'))
       local check = logout:read('*all')
@@ -73,13 +76,16 @@ end
 -- GETSTATE
 function getState()
    if debug == 1 then print('start getState') end
-   local uuId = uuid()
-   local d4 = '\"random='..uuId..'&clientId='..clientId..'&clientIdChecksum='..clientIdChecksum..'\"'  
+   local tmp3 = uuid()
+   --if debug == 1 then print('getstate - uuid: '..uuid4) end
+   local d4 = '\"random='..uuid4..'&clientId='..clientId..'&clientIdChecksum='..clientIdChecksum..'\"'  
    if debug == 1 then print('body getState: '..d4) end
    getState = assert(io.popen('curl -X GET -d '..d4..' https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/retrieveToonState'))
    local state = getState:read('*all')
    print('getstate: '..state)
       getState:close()
+      jsonState, pos, err = json.decode(state, 1, nil)
+	   if jsonState.success == false then print("Did not retreive Toon state") end
 end
 
 commandArray = {}
@@ -87,5 +93,6 @@ if devicechanged['testSwitch'] then
    local test1 = login()
    local test2 = getState()
    local test3 = logout()
+   --local test = uuid()
 end
 return commandArray
